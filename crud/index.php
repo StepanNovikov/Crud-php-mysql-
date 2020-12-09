d5d48c9edd994a74bfaf36ea476d66513b027c11
Stepan <stepan.novikov.1996.com>
2020-12-09 23:56:57 +0300

    created a new template on bootstrap

---
+++ crud/index.php
@@ -0,0 +1,58 @@
+<?php
+    require_once("../crud/php/component.php");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Books</title>
+    
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <!--Custom css-->
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+    <main>
+        <div class="container text-center">
+            <h1 class="py-4 bg-dark text-light rounded"><i class="fas fa-swatchbook">Book Store</i></h1>
+
+            <div class="d-flex justify-content-center">
+                <form action="" method="post" class="w-50">
+                    <div class="py-2">
+                        <?php inputElemenet("<i class='fas fa-id-badge'></i>", "ID","book_id",""); ?>
+                    </div>
+                    <div class="pt-2">
+                        <?php inputElemenet("<i class='fas fa-book'></i>", "Book Name","book_name",""); ?>
+                    </div>
+                    <div class="row pt-2">
+                        <div class="col">
+                            <?php inputElemenet("<i class='fas fa-people-carry'></i>", "Publisher","book_publisher",""); ?>
+                        </div>
+                        <div class="col">
+                            <?php inputElemenet("<i class='fas fa-dollar-sign'></i>", "Price","book_price",""); ?>
+                        </div>
+                    </div>
+                    <div class="d-flex">
+                        <?php buttonElement("btn-create","btn btn-success", "<i class='fas fa-plus'></i>", "create","data-toggle='tooltip' data-placement='bottom' title='Create'") ?>
+                        <?php buttonElement("btn-read","btn btn-primary", "<i class='fas fa-sync'></i>", "read","data-toggle='tooltip' data-placement='bottom' title='Read'") ?>
+                        <?php buttonElement("btn-update","btn btn-light border", "<i class='fas fa-pen-alt'></i>", "update","data-toggle='tooltip' data-placement='bottom' title='Update'") ?>
+                        <?php buttonElement("btn-delete","btn btn-danger", "<i class='fas fa-trash-alt'></i>", "delete","data-toggle='tooltip' data-placement='bottom' title='Delete'") ?>
+                    </div>
+                    </div>
+                    
+                </form>
+            </div>
+        </div>
+    </main>
+
+
+    <script src="https://kit.fontawesome.com/955f3ae4c3.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ crud/php/component.php
@@ -0,0 +1,20 @@
+<?php
+    function inputElemenet($icon,$placeholder, $name,$value){
+        $element = "
+            <div class=\"input-group mb-2\">
+            <div class=\"input-group-prepend\">
+                <div class=\"input-group-text bg-warning\">$icon</div>
+            </div>
+                <input type=\"text\" name='$name' value='$value' autocomplete=\"off\" placeholder='$placeholder' class=\"form-control\" id=\"inlineFormInputGroup\" placeholder=\"Username\">
+            </div>
+        ";
+        echo $element;
+    }
+
+    function buttonElement($btnId,$styleclass,$text,$name,$attr){
+        $btn = "
+            <button name='$name' '$attr' class='$styleclass' id='$btnid'>$text</button>
+        ";
+
+        echo $btn;
+    }
